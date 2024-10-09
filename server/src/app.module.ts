617636cc83f7374d28455dc364d3b3ef268dd2b2
Vladimir Linkevich <stipafk@gmail.com>
2024-10-09 20:49:00 +0300

    init heroku app

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MessageGateway } from './gateways/message.gateway';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 const ENV = process.env.NODE_ENV;
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'), // <-- path to the static files
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: !ENV ? '.env' : `.env.${ENV}`,
